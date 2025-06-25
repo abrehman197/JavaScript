6481ec80d37fd6d5341b284b17704f773a7cd087
abrehman197 <231370197@gift.edu.pk>
2025-06-25 13:32:27 +0000

    if lese

---
+++ 01_basics/ifElse.js
@@ -0,0 +1,55 @@
+// const isLogged = true
+
+// if (isLogged) {
+//     const name = "MANI"
+//     console.log("Logged In");
+// }else{
+//     console.log("Not a perfect match!");
+// }
+//console.log(name); scope error
+// <, <=, >, >=, ==, !=, ===, !==
+
+
+// const number = 1200
+// if (number > 700) console.log("Test"), console.log("Test2"); 
+
+// if (number == 1000 && number < 1100) {
+//     console.log("1st Case");
+// } else {
+//     console.log("2nd Case");
+// }
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//         break;
+//     case 2:
+//         console.log("Feb");
+//         break;
+//     case 3:
+//         console.log("Mar");
+//         break;
+//     default:
+//         break;
+// }
+
+
+// const mail = "mani@gmail.com"  if it empty else will execute
+
+// if (mail) {
+//     console.log("Have a mail");
+// }else{
+//     console.log("Not a mail");
+// }
+
+//False Values
+// false, 0, -0, "", null, 0n, undefined, NaN
+
+//True values
+// true, "0", 'false', [], {}, function(){}
+
+
+// ternary operator
+//condition ? true : false
